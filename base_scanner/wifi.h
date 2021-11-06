550512c4544bc10c5f36f5dd166b165f4e11a843
John <john.olivierb@gmail.com>
2021-11-05 22:52:04 -0400

    Changed the main file name

---
+++ base_scanner/wifi.h
@@ -0,0 +1,9 @@
+#ifndef WIFI_H
+#define WIFI_H
+
+const String local_pc_ip = "192.168.86.28";
+const String local_pc_port = "8080";
+const char * ssid = "jb-room";
+const char * password = "barry6405";
+
+#endif
